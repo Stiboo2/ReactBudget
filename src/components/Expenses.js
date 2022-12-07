45f5fe31f3d7268307a400bd1e129d1fd1bd34a8
Stiboo2 <thabo.radebe@live.com>
2022-12-07 15:17:28 +0200

    amend shortend

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,30 +1,19 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'; 
 
-function Expenses(props) {
+
+
+const Expenses = props => {
   return (
-    <dev className= "expenses">
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      ></ExpenseItem>
-    </dev>
+    <div className="expenses">
+      <h2> Lets get started! </h2>
+      {props.expenses.map(({ id, title, amount, date }) => (
+        <ExpenseItem title={title} amount={amount} date={date} />
+      ))}
+    </div>
   );
-}
+};
+
+
+
 export default Expenses;
